81a9c3f1d628c5d1222a6f073c709629e2dbce3f
unknown <nasshamid@hotmail.fr>
2023-02-06 19:10:26 +0100

    shopping cart 1/2

--- produits.js
+++ produits.js
@@ -1,68 +1,136 @@
-function saveBasket(basket) {
-    localStorage.setItem("basket", JSON.stringify(basket));
+let carts = document.querySelectorAll('.boutonBoutique');
+
+let products = [
+    {
+        name: "Cerisier",
+        tag: "Le Cerisier",
+        price: 2.99,
+        inCart: 0
+    },
+    {
+        name: "Coulis Caramel",
+        tag: "Le Coulis Caramel",
+        price: 13.99,
+        inCart: 0
+    },
+    {
+        name: "Douceur exotique",
+        tag: "La Douceur exotique",
+        price: 4.99,
+        inCart: 0
+    },
+    {
+        name: "Blanc Passion",
+        tag: "Le Blanc Passion",
+        price: 5.99,
+        inCart: 0
+    },
+    {
+        name: "Douceur croquante",
+        tag: "La Douceur croquante",
+        price: 2.99,
+        inCart: 0
+    },
+    {
+        name: "Zèbre",
+        tag: "Le Zèbre",
+        price: 19.99,
+        inCart: 0
+    },
+    {
+        name: "Coulis Fraise",
+        tag: "Le Coulis de Fraise",
+        price: 9.99,
+        inCart: 0
+    },
+    {
+        name: "Noir Passion",
+        tag: "Le Noir Passion",
+        price: 4.99,
+        inCart: 0
+    },
+    {
+        name: "Fantaisie",
+        tag: "La Fantaisie",
+        price: 2.99,
+        inCart: 0
+    },
+    {
+        name: "Caramel Salé",
+        tag: "Le Caramel Salé",
+        price: 14.99,
+        inCart: 0
+    },
+]
+
+for (let i=0; i< carts.length; i++){
+   carts[i].addEventListener('click', () => {
+    cartNumbers( products[i]);
+    totalCost(products[i]);
+   })
 }
 
-function getBasket() {
-    let basket = localStorage.getItem("basket");
-    if (basket == null) {
-            return [];
-    } else {
-            return JSON.parse(basket);
+function onLoadCartNumbers() {
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
     }
+
 }
 
 
-function addBasket(product) {
-    let basket = getBasket();
-    let foundProduct = basket.find(p=>p.id == product.id);
-    if (foundProduct != undefined) {
-        foundProduct.quantity++;
-    } else {
-        product.quantity = 1;
-        basket.push(product);
-    }
-    saveBasket(basket);
-}
-
-function removeFromBasket(product) {
-    let basket = getBasket();
-    basket = basket.filter(p => p.id != product.id);
-    saveBasket(basket);
-}
-
-function changeQuantity(product, quantity) {
-    let basket = getBasket();
-    let foundProduct = basket.find(p=> p.id == product.id);
-    if (foundProduct != undefined) {
-        foundProduct.quantity += quantity;
-        if(foundProduct.quantity <= 0) {
-            removeFromBasket(foundProduct);
-        } else {
-            saveBasket(basket)
-        }
-    }
+function cartNumbers( product) {
+   
+    let productNumbers = localStorage.getItem('cartNumbers');
     
-}
+    productNumbers = parseInt(productNumbers);
+    
+   if( productNumbers ) {
+    localStorage.setItem('cartNumbers' , productNumbers + 1 );
+    document.querySelector('.cart span').textContent = productNumbers + 1;
+   } else {
+    localStorage.setItem('cartNumbers' , 1 );
+    document.querySelector('.cart span').textContent = 1;
+   }
 
-function getNumberProduct(){
-    let basket = getBasket();
-    let number = 0;
-    for (let product of basket) {
-        number += product.quantity;
+   setItems(product);
+};
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    if(cartItems != null) {
+        if(cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart +=1;
+    } else {
+      product.inCart = 1;
+        cartItems = {
+           [product.tag]: product
+           }
     }
-    return number;
-}
+    localStorage.setItem("productsInCart", JSON.stringify (cartItems ));
+};
 
-function getTotalPrice() {
-    let basket = getBasket();
-    let total = 0;
-    for (let product of basket) {
-        total += product.quantity * product.price;
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+        
+
+    if(cartCost != null) {
+        cartCost = parseInt(cartCost);     
+        localStorage.setItem('totalCost', cartCost + product.price);
+    } else {
+        localStorage.setItem("totalCost", product.price);
     }
-    return total;
+
+   
 }
 
-var ajouter = document.querySelector(".boutonBoutique");
-ajouter.addEventListener('click', function addBasket(product){
-  
-});
+
+onLoadCartNumbers() ;
